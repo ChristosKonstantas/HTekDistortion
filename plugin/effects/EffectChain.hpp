e4093dc7ff4beec44bd6d28f8d8e8808fb10d659
ChristosKonstantas <titokonstantas1@gmail.com>
2026-02-14 22:05:34 +0100

    Added: EffectChain (dsp routing layer between processor and effects)

---
+++ plugin/effects/EffectChain.hpp
@@ -0,0 +1,49 @@
+#ifndef EFFECT_CHAIN_HPP
+#define EFFECT_CHAIN_HPP
+
+#include <array>
+#include "Effect.hpp"
+
+class EffectChain
+{
+    public:
+        static constexpr int kMaxEffects = 8;
+
+        void clear() noexcept 
+        { 
+            count = 0; 
+        }
+
+        bool addEffect (IEffect* effect) noexcept
+        {
+            if (effect == nullptr || count >= kMaxEffects) 
+                return false;
+            
+            effects[count++] = effect;
+            return true;
+        }
+
+        void prepare (double sampleRate, int maxBlockSize, int numChannels)
+        {
+            for (int i = 0; i < count; ++i)
+                effects[i]->prepare (sampleRate, maxBlockSize, numChannels);
+        }
+
+        void reset()
+        {
+            for (int i = 0; i < count; ++i)
+                effects[i]->reset();
+        }
+
+        void process (juce::AudioBuffer<float>& buffer) noexcept
+        {
+            for (int i = 0; i < count; ++i)
+                effects[i]->process (buffer);
+        }
+
+    private:
+        std::array<IEffect*, kMaxEffects> effects {};
+        int count = 0;
+};
+
+#endif
