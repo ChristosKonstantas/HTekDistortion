f466d7f9123279b5d65537e57972878b60e108f1
ChristosKonstantas <titokonstantas1@gmail.com>
2026-02-15 21:54:31 +0100

    Added: plugin editor and processor definitions

--- plugin/src/PluginEditor.cpp
+++ plugin/src/PluginEditor.cpp
@@ -1,33 +1,116 @@
-#include "PluginProcessor.hpp"
 #include "PluginEditor.hpp"
 
-//==============================================================================
 HTekDistortionAudioProcessorEditor::HTekDistortionAudioProcessorEditor (HTekDistortionAudioProcessor& p)
-    : AudioProcessorEditor (&p), processorRef (p)
+: AudioProcessorEditor (&p), _proc (p)
 {
-    juce::ignoreUnused (processorRef);
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    styleSlider (_drive,        "Drive (dB)");
+    styleSlider (_preHPF,       "preHPF (Hz)");
+    styleSlider (_threshold,    "Threshold");
+    styleSlider (_knee,         "Knee");
+    styleSlider (_bias,         "Bias");
+    styleSlider (_postLPF,      "postLPF (Hz)");
+    styleSlider (_mix,          "Mix");
+    styleSlider (_output,       "Output (dB)");
+
+    styleLabel(_driveLabel,     "Drive (dB)");    addAndMakeVisible(_driveLabel);
+    styleLabel(_preHPFLabel,    "preHPF (Hz)");   addAndMakeVisible(_preHPFLabel);
+    styleLabel(_thresholdLabel, "Threshold");     addAndMakeVisible(_thresholdLabel);
+    styleLabel(_kneeLabel,      "Knee");          addAndMakeVisible(_kneeLabel);
+    styleLabel(_biasLabel,      "Bias");          addAndMakeVisible(_biasLabel);
+    styleLabel(_postLPFLabel,   "postLPF (Hz)");  addAndMakeVisible(_postLPFLabel);
+    styleLabel(_mixLabel,       "Mix");           addAndMakeVisible(_mixLabel);
+    styleLabel(_outputLabel,    "Output (dB)");   addAndMakeVisible(_outputLabel);
+
+    styleLabel(_oversamplingLabel, "Oversampling"); addAndMakeVisible(_oversamplingLabel);
+
+    addAndMakeVisible (_drive);
+    addAndMakeVisible (_preHPF);
+    addAndMakeVisible (_threshold);
+    addAndMakeVisible (_knee);
+    addAndMakeVisible (_bias);
+    addAndMakeVisible (_postLPF);
+    addAndMakeVisible (_mix);
+    addAndMakeVisible (_output);
+
+    _oversampling.addItem ("Off", 1);
+    _oversampling.addItem ("2x",  2);
+    _oversampling.addItem ("4x",  3);
+    _oversampling.addItem ("8x",  4);
+    _oversampling.setJustificationType (juce::Justification::centred);
+    addAndMakeVisible (_oversampling);
+
+    _aDrive     = std::make_unique<Attachment> (_proc.apvts, "driveDb",   _drive);
+    _apreHPF    = std::make_unique<Attachment> (_proc.apvts, "preHPFHz",  _preHPF);
+    _aThreshold = std::make_unique<Attachment> (_proc.apvts, "threshold", _threshold);
+    _aKnee      = std::make_unique<Attachment> (_proc.apvts, "knee",      _knee);
+    _aBias      = std::make_unique<Attachment> (_proc.apvts, "bias",      _bias);
+    _apostLPF   = std::make_unique<Attachment> (_proc.apvts, "postLPFHz", _postLPF);
+    _aMix       = std::make_unique<Attachment> (_proc.apvts, "mix",       _mix);
+    _aOutput    = std::make_unique<Attachment> (_proc.apvts, "outputDb",  _output);
+    _aOs        = std::make_unique<ChoiceAttachment> (_proc.apvts, "oversampling", _oversampling);
+
+    setSize (640, 320);
 }
 
-HTekDistortionAudioProcessorEditor::~HTekDistortionAudioProcessorEditor()
+void HTekDistortionAudioProcessorEditor::styleSlider (juce::Slider& s, const juce::String& name)
 {
+    s.setSliderStyle (juce::Slider::RotaryHorizontalVerticalDrag);
+    s.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 80, 18);
+    s.setName (name);
 }
 
-//==============================================================================
+void HTekDistortionAudioProcessorEditor::styleLabel(juce::Label& l, const juce::String& text)
+{
+    l.setText(text, juce::dontSendNotification);
+    l.setJustificationType(juce::Justification::centred);
+    l.setColour(juce::Label::textColourId, juce::Colours::white);
+}
+
 void HTekDistortionAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
+    g.fillAll (juce::Colours::black);
     g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Audio Plugin Template", getLocalBounds(), juce::Justification::centred, 1);
+    g.setFont (16.0f);
+    g.drawFittedText ("HTekDistortion", getLocalBounds().removeFromTop(24), juce::Justification::centred, 1);
 }
 
 void HTekDistortionAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    juce::Rectangle<int> r = getLocalBounds().reduced(10);
+
+    r.removeFromTop(24);
+
+    juce::Rectangle<int> bottomBar = r.removeFromBottom(30);
+    juce::Rectangle<int> topRow = r.removeFromTop(r.getHeight() / 2);
+    juce::Rectangle<int> botRow = r;
+
+    auto cell = [] (juce::Rectangle<int>& row, int n)
+    {
+        const int w = row.getWidth() / n;
+        return row.removeFromLeft(w).reduced(6);
+    };
+
+    auto placeKnobWithLabel = [] (juce::Slider& s, juce::Label& l, juce::Rectangle<int> area)
+    {
+        juce::Rectangle<int> labelArea = area.removeFromTop(18);
+        l.setBounds(labelArea);
+        s.setBounds(area);
+    };
+
+    juce::Rectangle<int> tr = topRow;
+    placeKnobWithLabel(_drive,     _driveLabel,     cell(tr, 4));
+    placeKnobWithLabel(_preHPF,    _preHPFLabel,    cell(tr, 4));
+    placeKnobWithLabel(_threshold, _thresholdLabel, cell(tr, 4));
+    placeKnobWithLabel(_knee,      _kneeLabel,      cell(tr, 4));
+
+    juce::Rectangle<int> br = botRow;
+    placeKnobWithLabel(_bias,    _biasLabel,    cell(br, 4));
+    placeKnobWithLabel(_postLPF, _postLPFLabel, cell(br, 4));
+    placeKnobWithLabel(_mix,     _mixLabel,     cell(br, 4));
+    placeKnobWithLabel(_output,  _outputLabel,  cell(br, 4));
+
+    // Oversampling label + box (algorithm pending)
+    juce::Rectangle<int> osArea = bottomBar.removeFromRight(220);
+    _oversamplingLabel.setBounds(osArea.removeFromLeft(110).reduced(2));
+    _oversampling.setBounds(osArea.reduced(2));
 }
--- plugin/src/PluginProcessor.cpp
+++ plugin/src/PluginProcessor.cpp
@@ -1,22 +1,34 @@
 #include "PluginProcessor.hpp"
 #include "PluginEditor.hpp"
+#include <cmath>
 
 //==============================================================================
 HTekDistortionAudioProcessor::HTekDistortionAudioProcessor()
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+   : AudioProcessor (BusesProperties()
+    #if ! JucePlugin_IsMidiEffect
+    #if ! JucePlugin_IsSynth
+                    .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+    #endif
+                    .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+    #endif
+                  )
+    , apvts (*this, nullptr, "PARAMS", createParameterLayout())
 {
+    _driveDb      = apvts.getRawParameterValue ("driveDb");
+    _preHPFHz     = apvts.getRawParameterValue ("preHPFHz");
+    _threshold    = apvts.getRawParameterValue ("threshold");
+    _knee         = apvts.getRawParameterValue ("knee");
+    _bias         = apvts.getRawParameterValue ("bias");
+    _postLPFHz    = apvts.getRawParameterValue ("postLPFHz");
+    _mix          = apvts.getRawParameterValue ("mix");
+    _outputDb     = apvts.getRawParameterValue ("outputDb");
+    _oversampling = apvts.getRawParameterValue ("oversampling");
+
+    _chain.clear();
+    _chain.addEffect (&_distortion);
 }
 
-HTekDistortionAudioProcessor::~HTekDistortionAudioProcessor()
-{
-}
+HTekDistortionAudioProcessor::~HTekDistortionAudioProcessor() = default;
 
 //==============================================================================
 const juce::String HTekDistortionAudioProcessor::getName() const
@@ -56,107 +68,90 @@ double HTekDistortionAudioProcessor::getTailLengthSeconds() const
     return 0.0;
 }
 
-int HTekDistortionAudioProcessor::getNumPrograms()
+int HTekDistortionAudioProcessor::getNumPrograms()           { return 1; }
+int HTekDistortionAudioProcessor::getCurrentProgram()        { return 0; }
+void HTekDistortionAudioProcessor::setCurrentProgram (int i) { juce::ignoreUnused (i); }
+const juce::String HTekDistortionAudioProcessor::getProgramName (int i)
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
-}
-
-int HTekDistortionAudioProcessor::getCurrentProgram()
-{
-    return 0;
-}
-
-void HTekDistortionAudioProcessor::setCurrentProgram (int index)
-{
-    juce::ignoreUnused (index);
-}
-
-const juce::String HTekDistortionAudioProcessor::getProgramName (int index)
-{
-    juce::ignoreUnused (index);
+    juce::ignoreUnused (i);
     return {};
 }
-
-void HTekDistortionAudioProcessor::changeProgramName (int index, const juce::String& newName)
+void HTekDistortionAudioProcessor::changeProgramName (int i, const juce::String& n)
 {
-    juce::ignoreUnused (index, newName);
+    juce::ignoreUnused (i, n);
+}
+
+//==============================================================================
+bool HTekDistortionAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+    const auto mainIn  = layouts.getMainInputChannelSet();
+    const auto mainOut = layouts.getMainOutputChannelSet();
+
+    if (mainOut.isDisabled()) return false;
+
+   #if ! JucePlugin_IsSynth
+    if (mainIn.isDisabled()) return false;
+    if (mainIn != mainOut)   return false;
+   #endif
+
+    return (mainOut == juce::AudioChannelSet::mono()
+         || mainOut == juce::AudioChannelSet::stereo());
 }
 
 //==============================================================================
 void HTekDistortionAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
-    juce::ignoreUnused (sampleRate, samplesPerBlock);
+    _maxBlockSize = samplesPerBlock;
+
+    const int numChannels = getTotalNumOutputChannels();
+    jassert (numChannels > 0);
+
+    _chain.prepare (sampleRate, samplesPerBlock , numChannels);
+    _chain.reset();
 }
 
 void HTekDistortionAudioProcessor::releaseResources()
 {
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
+    _chain.reset();
+    // oversampling.reset();
+    setLatencySamples (0);
 }
 
-bool HTekDistortionAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
-{
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
-
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
-
-    return true;
-  #endif
-}
-
+//==============================================================================
 void HTekDistortionAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
-                                              juce::MidiBuffer& midiMessages)
+                                                juce::MidiBuffer& midiMessages)
 {
     juce::ignoreUnused (midiMessages);
-
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+    const int numSamples = buffer.getNumSamples();
+    if (buffer.getNumChannels() == 0 || numSamples == 0)
+        return;
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-        juce::ignoreUnused (channelData);
-        // ..do something to the data...
-    }
+    for (int ch = getTotalNumInputChannels(); ch < getTotalNumOutputChannels(); ++ch)
+        buffer.clear (ch, 0, numSamples);
+
+    HTekDistortionEffect::Params p;
+    p.driveDb   = _driveDb->load();
+    p.preHPFHz  = _preHPFHz->load();
+    p.threshold = _threshold->load();
+    p.knee      = _knee->load();
+    p.bias      = _bias->load();
+    p.postLPFHz = _postLPFHz->load();
+    p.mix       = _mix->load();
+    p.outputDb  = _outputDb->load();
+
+    _distortion.setParams (p);
+
+    juce::dsp::AudioBlock<float> baseBlock (buffer);
+
+    _chain.process (baseBlock);
 }
 
 //==============================================================================
 bool HTekDistortionAudioProcessor::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+    return true;
 }
 
 juce::AudioProcessorEditor* HTekDistortionAudioProcessor::createEditor()
@@ -165,19 +160,71 @@ juce::AudioProcessorEditor* HTekDistortionAudioProcessor::createEditor()
 }
 
 //==============================================================================
+juce::AudioProcessorValueTreeState::ParameterLayout HTekDistortionAudioProcessor::createParameterLayout()
+{
+    using APVTS = juce::AudioProcessorValueTreeState;
+    std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
+
+    params.push_back (std::make_unique<juce::AudioParameterFloat>(
+        "driveDb", "Drive",
+        juce::NormalisableRange<float> (0.0f, 36.0f, 0.01f), 18.0f));
+
+    params.push_back (std::make_unique<juce::AudioParameterFloat>(
+        "preHPFHz", "preHPF (HPF)",
+        juce::NormalisableRange<float> (20.0f, 700.0f, 0.01f, 0.5f), 80.0f));
+
+    params.push_back (std::make_unique<juce::AudioParameterFloat>(
+        "threshold", "Threshold",
+        juce::NormalisableRange<float> (0.05f, 1.0f, 0.0001f), 0.7f));
+
+    params.push_back (std::make_unique<juce::AudioParameterFloat>(
+        "knee", "Knee",
+        juce::NormalisableRange<float> (0.0f, 0.80f, 0.0001f), 0.12f));
+
+    params.push_back (std::make_unique<juce::AudioParameterFloat>(
+        "bias", "Bias",
+        juce::NormalisableRange<float> (-0.5f, 0.5f, 0.0001f), 0.08f));
+
+    params.push_back (std::make_unique<juce::AudioParameterFloat>(
+        "postLPFHz", "postLPF (LPF)",
+        juce::NormalisableRange<float> (3000.0f, 20000.0f, 0.01f, 0.5f), 12000.0f));
+
+    params.push_back (std::make_unique<juce::AudioParameterFloat>(
+        "mix", "Mix",
+        juce::NormalisableRange<float> (0.0f, 1.0f, 0.0001f), 1.0f));
+
+    params.push_back (std::make_unique<juce::AudioParameterFloat>(
+        "outputDb", "Output",
+        juce::NormalisableRange<float> (-24.0f, 24.0f, 0.01f), -1.0f));
+    
+    //TODO: Add oversampling!
+    params.push_back (std::make_unique<juce::AudioParameterChoice>(
+        "oversampling", "Oversampling",
+        juce::StringArray { "Off", "2x", "4x", "8x" }, 0));
+
+    return { params.begin(), params.end() };
+}
+
+//==============================================================================
 void HTekDistortionAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
-    juce::ignoreUnused (destData);
+    auto state = apvts.copyState();
+    std::unique_ptr<juce::XmlElement> xml (state.createXml());
+    copyXmlToBinary (*xml, destData);
 }
 
 void HTekDistortionAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
-    juce::ignoreUnused (data, sizeInBytes);
+    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+    if (xmlState == nullptr) return;
+
+    if (xmlState->hasTagName (apvts.state.getType()))
+        apvts.replaceState (juce::ValueTree::fromXml (*xmlState));
 }
 
 //==============================================================================
