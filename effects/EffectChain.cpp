40f0a29512fe4cef53c2ee995679418c40dd8865
ChristosKonstantas <titokonstantas1@gmail.com>
2026-08-02 12:49:49 +0200

    Added: definitions for effect chain

--- effects/EffectChain.cpp
+++ effects/EffectChain.cpp
@@ -1,2 +1,43 @@
 #include "EffectChain.hpp"
-// TODO: (now is header-only)
+
+void EffectChain::clear() noexcept
+{
+    _count = 0;
+    _effects.fill(nullptr);
+}
+
+bool EffectChain::addEffect (IEffect* effect) noexcept
+{
+    jassert(effect != nullptr);
+    jassert(_count < numMaxEffects);
+
+    if (effect == nullptr || _count >= numMaxEffects)
+        return false;
+    
+    _effects[_count++] = effect;
+    return true;
+}
+
+void EffectChain::prepare (double sampleRate, int maxBlockSize, int numChannels)
+{
+    jassert(_count > 0);
+
+    for (int i = 0; i < _count; ++i)
+        _effects[i]->prepare (sampleRate, maxBlockSize, numChannels);
+}
+
+void EffectChain::reset()
+{
+    jassert(!_effects.empty());
+
+    for (int i = 0; i < _count; ++i)
+        _effects[i]->reset();
+}
+
+void EffectChain::process (juce::dsp::AudioBlock<float>& block) noexcept
+{
+    jassert(!_effects.empty());
+
+    for (int i = 0; i < _count; ++i)
+        _effects[i]->process (block);
+}
--- effects/EffectChain.hpp
+++ effects/EffectChain.hpp
@@ -9,47 +9,15 @@ class EffectChain
     public:
         static constexpr int numMaxEffects = 8;
 
-        void clear() noexcept
-        {
-            _count = 0;
-            _effects.fill(nullptr);
-        }
+        void clear() noexcept;
 
-        bool addEffect (IEffect* effect) noexcept
-        {
-            jassert(effect != nullptr);
-            jassert(_count < numMaxEffects);
+        bool addEffect(IEffect *effect) noexcept;
 
-            if (effect == nullptr || _count >= numMaxEffects)
-                return false;
-            
-            _effects[_count++] = effect;
-            return true;
-        }
+        void prepare(double sampleRate, int maxBlockSize, int numChannels);
 
-        void prepare (double sampleRate, int maxBlockSize, int numChannels)
-        {
-            jassert(_count > 0);
+        void reset();
 
-            for (int i = 0; i < _count; ++i)
-                _effects[i]->prepare (sampleRate, maxBlockSize, numChannels);
-        }
-
-        void reset()
-        {
-            jassert(!_effects.empty());
-
-            for (int i = 0; i < _count; ++i)
-                _effects[i]->reset();
-        }
-
-        void process (juce::dsp::AudioBlock<float>& block) noexcept
-        {
-            jassert(!_effects.empty());
-
-            for (int i = 0; i < _count; ++i)
-                _effects[i]->process (block);
-        }
+        void process(juce::dsp::AudioBlock<float> &block) noexcept;
 
     private:
         std::array<IEffect*, numMaxEffects> _effects {};
